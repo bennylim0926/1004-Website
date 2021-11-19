8d477891acd2b1bbac5347be4c5d1743df7beff0
bennylim0926 <bennylim0926@gmail.com>
2021-11-19 19:03:05 +0800

    update shopping cart page

--- 1004-Website/shopping_cart.php
+++ 1004-Website/shopping_cart.php
@@ -67,8 +67,52 @@
                             </button>
                         </td>
                     </tr>
+                     <tr>
+                        <td>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    Monitor QWERTY
+                                </label>
+                            </div>
+                        </td>
+                        <td class="text-right">$30.00</td>
+                        <td class="text-right">2</td>
+                        <td class="text-right">$60.00</td>
+                        <td class="text-right">
+                            <button class='btn btn-danger'>
+                                Remove
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td class="text-right"> </td>
+                        <td class="text-right"></td>
+                        <td class="text-right">Total(6 items): $360.00</td>
+                        <td class="text-right">
+                            <button class='btn btn-primary'>
+                                Check Out
+                            </button>
+                        </td>
+                    </tr>
                 </tbody>
-            </table>          
+            </table>  
+            <div>
+                <h3>YOU MAY ALSO LIKE</h3>
+                <figure style="display:inline;margin:auto">
+                    <img src="images/dogeLogo.jfif" alt="Poodle"
+                         title="View larger image..." />                                                                          
+                    <img src="images/dogeLogo.jfif" alt="Poodle"
+                         title="View larger image..." />                                                                            
+                    <img src="images/dogeLogo.jfif" alt="Poodle"
+                         title="View larger image..." />   
+                    <img src="images/dogeLogo.jfif" alt="Poodle"
+                         title="View larger image..." />  
+                    <img src="images/dogeLogo.jfif" alt="Poodle"
+                                 title="View larger image..." />       
+                </figure>
+            </div>
         </main>
         <?php
         include 'footer.inc.php';

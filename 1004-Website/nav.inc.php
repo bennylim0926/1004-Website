dca66ff14365ccfa0993433a08de753c623aefe7
2102596sit <2102596@sit.singaporetech.edu.sg>
2021-11-28 20:22:00 +0800

    Update nav.inc.php

--- 1004-Website/nav.inc.php
+++ 1004-Website/nav.inc.php
@@ -1,4 +1,7 @@
 
+<nav class="navbar navbar-expand-sm navbar-custom " >
+
+
     <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>

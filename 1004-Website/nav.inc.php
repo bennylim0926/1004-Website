cef636ba0ad44df99c98b2c364f0ebd85cb47ec5
2102596sit <2102596@sit.singaporetech.edu.sg>
2021-11-29 18:56:19 +0800

    changes to improve reusability
    changes to improve reusability

--- 1004-Website/nav.inc.php
+++ 1004-Website/nav.inc.php
@@ -28,6 +28,7 @@
         <ul class="nav navbar-nav navbar-right ml-auto">
             <li>
                 <?php
+                
                 if (isset($_SESSION["uname"])) {
                     $profile_pic = "";
                     include("Connection/connection.php");
@@ -36,10 +37,12 @@
                     include("Connection/handle_sql_execute_failure.php");
                     $conn->close();
                     $profile_pic = $result->fetch_assoc()["photo"];
+                    if($profile_pic == null)
+                        $profile_pic ="images/default_avatar.png";
                     echo "<li class='nav-item'> <img alt='Avatar' class='avatar' src='$profile_pic' id='avatarimg'> Welcome back, " . $_SESSION["uname"] . "</a></li>";
                     unset($profile_pic);
-
                     echo "<li class='nav-item'> <a class='nav-link' href='/1004-Website/edit_account.php'><span class='material-icons'>account_circle</span>Edit Account</a></li>";
+                    
                     if (($_SESSION['admin']) == true) {
                         echo "<li class='nav-item'> <a class='nav-link' href='/1004-Website/adminpage.php'><span class='material-icons'>account_circle</span>User management</a></li>";
                     }

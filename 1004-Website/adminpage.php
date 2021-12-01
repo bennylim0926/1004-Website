52e109500787045520e868162567fecdae1e4642
2102596sit <2102596@sit.singaporetech.edu.sg>
2021-12-01 22:26:40 +0800

    Update adminpage.php
    added missing class in css

--- 1004-Website/adminpage.php
+++ 1004-Website/adminpage.php
@@ -6,20 +6,13 @@ include('session/SessionCheckAdmin.php');
 include "head.inc.php";
 ?>
  
-<!--<head>
-    
-   Required meta tags 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-   Bootstrap CSS 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-  
-  <title>Admin User Management Page</title>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="css/main.css">
-
-</head>-->
-
+<style type="text/css">
+    .btnAdd {
+      text-align: right;
+      width: 83%;
+      margin-bottom: 20px;
+    }
+  </style>
 <body>
    <?php
         include "nav.inc.php";

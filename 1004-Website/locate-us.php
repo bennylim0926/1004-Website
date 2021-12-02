8d56fc1da1ed23bd027fee1a04549bf0155b48a2
2101035 <94104852+2101035@users.noreply.github.com>
2021-12-02 12:21:14 +0800

    edit css and index.php
    changed bg color some parts for index

--- 1004-Website/locate-us.php
+++ 1004-Website/locate-us.php
@@ -24,7 +24,7 @@ and open the template in the editor.
                     </div>
                     <div class="col-12 mapouter map-responsive">
                         <!--<div class="gmap_canvas">-->
-                            <iframe class="map-control" width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=SIT&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net">google maps create map</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
+                            <iframe title="embedded map frame" class="map-control" width="600" height="500" id="gmap_canvas" alt="embedded map" src="https://maps.google.com/maps?q=SIT&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net">google maps create map</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
                         <!--</div>-->
                     </div>
                 </div>
